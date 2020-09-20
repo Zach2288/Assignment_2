bfe225a280e82ddc25be896ee5eef2562c37d10c
Zachary Castonguay <12ZJC1@michener.ca>
2020-09-20 17:06:03 -0400

    Add assignment_2 script and gitignore

---
+++ assignment_2.py
@@ -0,0 +1,11 @@
+'''
+Assignment 2
+
+A filler program to show that I can create a repository
+'''
+
+#user name input
+name = input('What is your name?: ')
+
+#greeting
+print(f'Hello {name}. It\'s nice to meet you!')
